f0f1bc360e5cf0868f65a31bd754d370e3d4ad3b
pritam180802 <125137734+pritam180802@users.noreply.github.com>
2025-03-15 20:15:22 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Chocolate Distribution Problem/chocolate-distribution-problem.cpp
@@ -0,0 +1,48 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+class Solution {
+  public:
+    int findMinDiff(vector<int>& a, int m) {
+        sort(a.begin(),a.end());
+        int diff=INT_MAX;
+        for(int i=0;i<=a.size()-m;i++){
+            if((a[m+i-1]-a[i])<diff)
+              diff = a[m+i-1]-a[i];
+        }
+        return diff;
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    cin.ignore();
+    while (t--) {
+        vector<int> arr;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+
+        int m;
+        cin >> m;
+        cin.ignore();
+        Solution ob;
+        cout << ob.findMinDiff(arr, m) << endl;
+        cout << "~" << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
