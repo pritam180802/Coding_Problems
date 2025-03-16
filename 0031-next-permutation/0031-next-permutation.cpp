acbed1aee7054b41a71e55afcbcb7c059b90b8ac
pritam180802 <125137734+pritam180802@users.noreply.github.com>
2025-03-16 09:25:41 +0530

    Time: 0 ms (100%), Space: 15.7 MB (33.23%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.cpp
+++ 0031-next-permutation/0031-next-permutation.cpp
@@ -3,7 +3,7 @@ public:
     void nextPermutation(vector<int>& nums) {
         int n = nums.size();
         int breakpoint = -1;
-
+        // Finding Breakpoint
         for(int i=n-2;i>=0;i--)
         {
             if(nums[i]<nums[i+1]){
@@ -12,12 +12,13 @@ public:
             }
                
         }
-
+        // Checking whether there is any breakpoint or not!!!
         if(breakpoint == -1)
         {
-            reverse(nums.begin(),nums.end());
+            reverse(nums.begin(),nums.end()); // If there is no breakpoint
             return;
         }
+        //If the breakpoint found !!!
         int j;
         for(j=n-1;j>breakpoint;j--)
         {
